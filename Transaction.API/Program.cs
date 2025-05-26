39674e0768c1920abd49ad774e8bdd68dee263b9
aestdile@gmail.com <aestdile@gmail.com>
2025-05-26 17:19:46 +0500

    Created Auth and Autf

--- Transaction.API/Program.cs
+++ Transaction.API/Program.cs
@@ -1,4 +1,8 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Transaction.API.DataAcces;
 using Transaction.API.DataAccess;
 using Transaction.API.DataAccess.Entities;
@@ -12,6 +16,30 @@ builder.Services.AddDbContextPool<AppDbContext>(
     options => options.UseSqlServer(
         builder.Configuration.GetConnectionString("TransactionDb")));
 
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["JWT:ValidAudience"],
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+        };
+    });
+
 builder.Services.AddScoped<IGenericCRUDService<BankAccountModel>, BankAccounCRUDService>();
 builder.Services.AddScoped<IGenericCRUDService<CustomerModel>, CustomerCRUDService>();
 
@@ -23,6 +51,17 @@ builder.Services.AddScoped<IGenericRepository<Customer>, CustomerRepository>();
 builder.Services.AddScoped<IAccountNumberValidationService, AccountNumberValidationService>();
 builder.Services.AddScoped<ITransactionService, TransactionService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -32,6 +71,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
